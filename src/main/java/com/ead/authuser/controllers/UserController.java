cabd7fef956418f8f7706b4846ad6544f700797c
Pedro Furtado <furtado.pedropaulo@gmail.com>
2024-11-17 22:33:35 -0300

    feat: validation

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -27,19 +28,19 @@ public class UserController {
     private final UserServices userServices;
 
     @GetMapping
-    public ResponseEntity<List<UserModel>> getAllUser(){
+    public ResponseEntity<List<UserModel>> getAllUser() {
         return ResponseEntity.ok(userServices.findAll());
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId){
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userServices.findById(userId);
         return userModelOptional.<ResponseEntity<Object>>map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(USER_NOT_FOUND));
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<Object> deleteUSer(@PathVariable(value = "userId") UUID userId){
+    public ResponseEntity<Object> deleteUSer(@PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userServices.findById(userId);
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not fount");
@@ -49,8 +50,9 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<Object> putUser(@RequestBody @JsonView(UserDto.UserView.UserPut.class) UserDto userDto,
-                                          @PathVariable(value = "userId") UUID userId){
+    public ResponseEntity<Object> putUser(@RequestBody @Validated(UserDto.UserView.UserPut.class)
+                                          @JsonView(UserDto.UserView.UserPut.class) UserDto userDto,
+                                          @PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userServices.findById(userId);
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(USER_NOT_FOUND);
@@ -62,15 +64,16 @@ public class UserController {
     }
 
     @PutMapping("/{userId}/password")
-    public ResponseEntity<Object> putdatePassword(@RequestBody @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto,
-                                          @PathVariable(value = "userId") UUID userId){
+    public ResponseEntity<Object> putdatePassword(@RequestBody @Validated(UserDto.UserView.PasswordPut.class)
+                                                  @JsonView(UserDto.UserView.PasswordPut.class) UserDto userDto,
+                                                  @PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userServices.findById(userId);
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(USER_NOT_FOUND);
         }
-        if (Objects.equals(userModelOptional.get().getPassword(), userDto.getOldPassword())){
+        if (Objects.equals(userModelOptional.get().getPassword(), userDto.getOldPassword())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Mismatched old password ");
-        }else {
+        } else {
             BeanUtils.copyProperties(userDto, userModelOptional);
             userModelOptional.get().setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
             userServices.save(userModelOptional.get());
@@ -79,12 +82,13 @@ public class UserController {
     }
 
     @PutMapping("/{userId}/image")
-    public ResponseEntity<Object> putdateImage(@RequestBody @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto,
-                                                  @PathVariable(value = "userId") UUID userId){
+    public ResponseEntity<Object> putdateImage(@RequestBody @Validated(UserDto.UserView.ImagePut.class)
+                                               @JsonView(UserDto.UserView.ImagePut.class) UserDto userDto,
+                                               @PathVariable(value = "userId") UUID userId) {
         Optional<UserModel> userModelOptional = userServices.findById(userId);
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(USER_NOT_FOUND);
-        }else {
+        } else {
             BeanUtils.copyProperties(userDto, userModelOptional.get());
             userModelOptional.get().setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
             userServices.save(userModelOptional.get());
